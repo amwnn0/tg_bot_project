1635504880dca537b912ac3041d01942106398a0
Aleksandr_K <amwnn@mail.ru>
2025-08-21 20:57:22 +0300

    fix: reworked load_config as a function

--- config.py
+++ config.py
@@ -19,25 +19,17 @@ class Config:
     bot: TgBot
     db: DataBaseConfig
 
-load_dotenv()
-
-config = Config(
-    bot=TgBot(
-        token=os.getenv('TOKEN'),
-        admin_ids=os.getenv('ADMIN_IDS').split(',')
-    ),
-    db=DataBaseConfig(
-        name=os.getenv('DB_NAME'),
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD')
+def load_config() -> Config:
+    load_dotenv()
+    return Config(
+        bot=TgBot(
+            token=os.getenv('TOKEN'),
+            admin_ids=os.getenv('ADMIN_IDS').split(',')
+        ),
+        db=DataBaseConfig(
+            name=os.getenv('DB_NAME'),
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
+        )
     )
-)
-
-print('BOT_TOKEN:', config.bot.token)
-print('ADMIN_IDS:', config.bot.admin_ids)
-print()
-print('DB_NAME:', config.db.name)
-print('DB_HOST:', config.db.host)
-print('DB_USER:', config.db.user)
-print('DB_PASSWORD:', config.db.password)
